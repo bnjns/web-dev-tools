b61dd9e7edef9a4632c3b857d1d1554800653d34
Ben Jones <ben@bnjns.uk>
2017-07-31 23:40:23 +0100

    Allow customisation of attribute to use for status checking

--- src/Auth/LaravelStatusedUserProvider.php
+++ src/Auth/LaravelStatusedUserProvider.php
@@ -52,7 +52,7 @@ class LaravelStatusedUserProvider extends EloquentUserProvider
      */
     private function checkUserStatus($user)
     {
-        if($user && !$user->status) {
+        if($user && !$user->attributes[$user->statusAttribute ?: 'status']) {
             session()->flush();
             session()->regenerate();
             

4ca4270a5c3a0eb72242e07cfb367f089b6a3260
Ben Jones <ben@bnjns.uk>
2017-08-23 18:50:12 +0100

    Add a trait for automatically authorising with Gates

---
+++ src/Laravel/Traits/AuthorisesGates.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace bnjns\WebDevTools\Laravel\Traits;
+
+trait AuthorisesGates
+{
+    /**
+     * Create a method similar to authorize() for use with Gates.
+     *
+     * @param $action
+     *
+     * @throws \Illuminate\Auth\Access\AuthorizationException
+     */
+    protected function authorizeGate($action)
+    {
+        $denied = call_user_func_array('Gate::denies', func_get_args());
+        if ($denied) {
+            throw new AuthorizationException();
+        }
+    }
+}

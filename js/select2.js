8cea2f3b53b4ae9ccb0c31d6c24fa730a652fd5a
Ben Jones <ben@bnjns.uk>
2017-08-30 16:43:42 +0100

    Remove custom attribute processing - use data- instead

--- js/select2.js
+++ js/select2.js
@@ -1,20 +1,9 @@
 function select2(element, options) {
-    var select2_attributes = {};
-    var regexp             = new RegExp('select2\-(.*)', 'g');
-    var regexp_result;
-    jQuery.each(element.attributes, function(i, attribute) {
-        regexp_result = regexp.exec(attribute.name);
-        if (regexp_result != null) {
-            select2_attributes[regexp_result[1].toCamelCase()] = attribute.value;
-        }
-    });
-
-    $(element).select2(
+    element.select2(
         jQuery.extend({
                 theme     : 'bootstrap',
                 allowClear: false,
             },
-            typeof(options) == 'object' ? options : {},
-            select2_attributes),
+            typeof(options) == 'object' ? options : {}),
     );
 }

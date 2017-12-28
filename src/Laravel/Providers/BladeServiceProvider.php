9909113325425738ef5fc34b6ca1141f61a9d858
Ben Jones <ben@bnjns.uk>
2017-12-28 00:13:29 +0000

    Update Blade directives to be Bootstrap 4 compatible

--- src/Laravel/Providers/BladeServiceProvider.php
+++ src/Laravel/Providers/BladeServiceProvider.php
@@ -17,11 +17,8 @@ class BladeServiceProvider extends ServiceProvider
         Blade::directive('HelpDoc', function ($path) {
             return "<?php echo \Markdown::convertToHtml(file_get_contents(base_path('resources/documentation/' . str_replace('.', '/', {$path}) . '.md'))); ?>";
         });
-        Blade::directive('InputClass', function ($name) {
-            return "<?php echo \$errors->any() ? (\$errors->default->has({$name}) ? 'has-error' : 'has-success') : ''; ?>";
-        });
         Blade::directive('InputError', function ($name) {
-            return "<?php echo \$errors->any() && \$errors->default->has({$name}) ? ('<p class=\"help-block\">' . \$errors->default->first({$name}) . '</p>') : ''; ?>";
+            return "<?php echo \$errors->any() && \$errors->default->has({$name}) ? ('<div class=\"invalid-feedback\">' . \$errors->default->first({$name}) . '</div>'): ''; ?>";
         });
         Blade::directive('Paginator', function ($name) {
             return "<?php echo get_class({$name}) == 'Illuminate\Pagination\LengthAwarePaginator' ? {$name} : ''; ?>";

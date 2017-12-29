bf655b7fca6b7d6f58d95be0534b55d7ec81fda8
Ben Jones <ben@bnjns.uk>
2017-12-29 02:35:01 +0000

    Allow customisation of pagination view

--- src/Laravel/Providers/BladeServiceProvider.php
+++ src/Laravel/Providers/BladeServiceProvider.php
@@ -20,8 +20,9 @@ class BladeServiceProvider extends ServiceProvider
         Blade::directive('InputError', function ($name) {
             return "<?php echo \$errors->any() && \$errors->default->has({$name}) ? ('<div class=\"invalid-feedback\">' . \$errors->default->first({$name}) . '</div>'): ''; ?>";
         });
-        Blade::directive('Paginator', function ($name) {
-            return "<?php echo get_class({$name}) == 'Illuminate\Pagination\LengthAwarePaginator' ? {$name} : ''; ?>";
+        Blade::directive('Paginator', function ($arguments) {
+            list($name, $style) = array_pad($this->getDirectiveArguments($arguments), 2, null);
+            return "<?php echo get_class({$name}) == 'Illuminate\Pagination\LengthAwarePaginator' ? {$name}->render('pagination::" . ($style ?: 'default') . "') : ''; ?>";
         });
         Blade::directive('ContentWidth', function () {
             return "<?php echo !empty(trim(\$__env->yieldContent('content-width'))) ? ('w-' . \$__env->yieldContent('content-width')) : ''; ?>";
@@ -36,5 +37,17 @@ class BladeServiceProvider extends ServiceProvider
     public function register()
     {
 
+    }
+
+    /**
+     * Convert a string of arguments to an array.
+     *
+     * @param string $arguments
+     *
+     * @return array
+     */
+    private function getDirectiveArguments($arguments)
+    {
+        return explode(',', str_replace(['(', ')', ' ', "'"], '', $arguments));
     }
 }

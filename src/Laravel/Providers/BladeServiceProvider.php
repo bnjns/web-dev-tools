3dece7db38fbe032efa20fe1ba9ce93215e7b5e7
Ben Jones <ben@bnjns.uk>
2018-04-22 14:33:52 +0100

    Bootstrap 3 and 4 compatibility

--- src/Laravel/Providers/BladeServiceProvider.php
+++ src/Laravel/Providers/BladeServiceProvider.php
@@ -17,12 +17,18 @@ class BladeServiceProvider extends ServiceProvider
         Blade::directive('HelpDoc', function ($path) {
             return "<?php echo \Markdown::convertToHtml(file_get_contents(base_path('resources/documentation/' . str_replace('.', '/', {$path}) . '.md'))); ?>";
         });
+        Blade::directive('InputClass', function ($name) {
+            return "<?php echo \$errors->any() ? (\$errors->default->has({$name}) ? 'has-error' : 'has-success') : ''; ?>";
+        });
         Blade::directive('InputError', function ($name) {
-            return "<?php echo \$errors->any() && \$errors->default->has({$name}) ? ('<div class=\"invalid-feedback\">' . \$errors->default->first({$name}) . '</div>'): ''; ?>";
+            return "<?php echo \$errors->any() && \$errors->default->has({$name}) ? ('<div class=\"invalid-feedback help-block\">' . 
+\$errors->default->first({$name}) . '</div>'): ''; ?>";
         });
         Blade::directive('Paginator', function ($arguments) {
             list($name, $style) = array_pad($this->getDirectiveArguments($arguments), 2, null);
-            return "<?php echo get_class({$name}) == 'Illuminate\Pagination\LengthAwarePaginator' ? {$name}->render('pagination::" . ($style ?: 'default') . "') : ''; ?>";
+            return "<?php echo get_class({$name}) == 'Illuminate\Pagination\LengthAwarePaginator' ? {$name}->render('pagination::" .
+                   ($style ?: 'default') .
+                   "') : ''; ?>";
         });
         Blade::directive('ContentWidth', function () {
             return "<?php echo !empty(trim(\$__env->yieldContent('content-width'))) ? ('w-' . \$__env->yieldContent('content-width')) : ''; ?>";

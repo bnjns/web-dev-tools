cb492d38b1db3be122e91dc21312dcbbb4073f04
Ben Jones <ben@bnjns.uk>
2018-04-22 17:48:55 +0100

    Fix select2 to apply to jQuery object

--- js/plugins/ApplySelect2/enable.js
+++ js/plugins/ApplySelect2/enable.js
@@ -1,7 +1,7 @@
 (function($) {
     $.fn.ApplySelect2 = function(options) {
         this.each(function(i, element) {
-            element.select2(jQuery.extend({
+            $(element).select2(jQuery.extend({
                     theme     : 'bootstrap',
                     allowClear: false,
                 },
